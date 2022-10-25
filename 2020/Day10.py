390433f508863841d06575482778ba69a2b058f5
kkincaid618 <klayers618@gmail.com>
2022-10-25 00:04:28 -0500

    start at the end

--- 2020/Day10.py
+++ 2020/Day10.py
@@ -2,18 +2,24 @@ path = './2020/Data/Day10Data.txt'
 f = open(path, 'r')
 values = f.read().split('\n')
 adapters = [int(v) for v in values]
+adapters.append(0)
 adapters.append(max(adapters) + 3)
-adapters.sort()
+adapters.sort(reverse=True)
 
-differences = [adapters[0] - 0]
+print(adapters)
+
+differences = []
 
 for i in range(len(adapters) - 1):
     adapter = adapters[i]
-    min_possible = adapter + 1
-    max_possible = adapter + 3
+    max_possible = adapter - 1
+    min_possible = adapter - 3
+    
     all_possible = [a for a in adapters if a in list(range(min_possible, max_possible + 1))]
-    selected_adapter = min(all_possible)
-    diff = selected_adapter - adapter
+    num_possible = len(all_possible)
+    
+    selected_adapter = max(all_possible)
+    diff = adapter - selected_adapter
     differences.append(diff)
 
     # print(f'{adapter} => {selected_adapter} => {differences}')
@@ -21,4 +27,4 @@ for i in range(len(adapters) - 1):
 one_volt_diffs = len([d for d in differences if d == 1])
 three_volt_diffs = len([d for d in differences if d == 3])
 
-print(f'Part 1 Solution: {three_volt_diffs * one_volt_diffs}')
+print(f'Part 1 Solution: {three_volt_diffs} * {one_volt_diffs} = {three_volt_diffs * one_volt_diffs}')
